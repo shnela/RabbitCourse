0595fa8ac56e8a96418b5b2635adb1508ce2db66
jkushner <jakub.kuszneruk@neptune.ai>
2022-10-03 20:26:19 +0200

    Add StartupObject properties and sample solution for 05.md

--- 05_connect_using_C#_client.md
+++ 05_connect_using_C#_client.md
@@ -7,6 +7,9 @@
 [.NET/C# RabbitMQ Client Library](https://rabbitmq.com/dotnet.html)
 
 ## Tasks
+**All below tasks are based no code located in:**
+* **`cd src/Csharp/Producer`**
+* and **`cd src/Csharp/Consumer`**
 
 ### Publish messages
 1. Open new terminal: `Top menu` &rarr; `Terminal` &rarr; `New terminal`
@@ -22,3 +25,9 @@
    1. **The code will result in error because we're trying to declare wrong type of the queue**
    2. Comment out line responsible for declaring the queue and run the code.
    3. Now it should work, but assuming that the queue existss is a bad practice. Fix declaration of the queue, so it'll match declared queue parameters.
+   4. In case of problems:
+      1. you can compare file with `Consumer05.cs`
+      2. or just run `Consumer05.cs` by changing `<StartupObject>` property in `consumer.csproj`
+
+## More about RabbitMQ.Client
+[ .NET/C# Client API Guide ](https://rabbitmq.com/dotnet-api-guide.html)
---
+++ src/Csharp/Consumer/Consumer05.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+class Consumer05
+{
+    private const string QueueName = "q.test1.from_code";
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: QueueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: QueueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" * Received {0}", message);
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
