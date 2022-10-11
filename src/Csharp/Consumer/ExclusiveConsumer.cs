12b8610175c77016c90b4f8203ff131009b65a67
jkushner <jakub.kuszneruk@neptune.ai>
2022-10-11 21:30:30 +0200

    Fanout example

---
+++ src/Csharp/Consumer/ExclusiveConsumer.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class ExclusiveConsumer
+{
+    private const string ExchangeName = "ex.code.fanout";
+
+    public static void Main(string[] args)
+    {
+        int msgs_per_second = Int32.Parse(args[0]);
+        consume(msgs_per_second);
+    }
+
+    public static void consume(int msgs_per_second)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            ClientProvidedName = "exclusive_consumer_connection"
+        };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            var declaredQueue = channel.QueueDeclare(
+                queue: "",
+                durable: false,
+                exclusive: true,
+                autoDelete: false
+            );
+
+            channel.QueueBind(
+                queue: declaredQueue.QueueName,
+                exchange: ExchangeName,
+                routingKey: ""
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: declaredQueue.QueueName, autoAck: true, consumer: consumer);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("[{0}] * Received {1}", DateTime.Now.TimeOfDay, message);
+                Thread.Sleep(1000 / msgs_per_second);
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Csharp/Producer/FanoutProducer.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class FanoutProducer
+{
+    private const string ExchangeName = "ex.code.fanout";
+
+    public static void Main(string[] args)
+    {
+        int msgs_per_second = Int32.Parse(args[0]);
+        produce(msgs_per_second);
+    }
+
+    public static void produce(int msgs_per_second)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            ClientProvidedName = "producer_connection"
+        };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(
+                exchange: ExchangeName,
+                type: "fanout",
+                durable: true,
+                autoDelete: false
+            );
+
+            while (true)
+            {
+                string message = "Hello World! " + DateTime.Now.TimeOfDay;
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: ExchangeName,
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body
+                );
+                Console.WriteLine(" * Sent {0}", message);
+                Thread.Sleep(1000 / msgs_per_second);
+            }
+        }
+    }
+}
